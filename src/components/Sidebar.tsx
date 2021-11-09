5c85e7aa252b131b0709feb84d062f90e1dfb5ba
Gabriel Fernandes <bielfer18@gmail.com>
2021-11-09 00:56:29 -0300

    fix: link to twitter and personal website

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -29,9 +29,9 @@ const Sidebar: FC = () => {
         alt="User Image"
       />
       <div className="w-full px-3 text-white">
-        <h1 className="text-3xl italic">{user.name || "Nome Indisponível"}</h1>
+        {user.name ? <h1 className="text-3xl italic">{user.name}</h1> : <></>}
         <p className="italic text-xl">@{username}</p>
-        <p className="text-gray-400 py-4">{user.bio}</p>
+        {user.bio ? <p className="text-gray-400 py-4">{user.bio}</p> : <></>}
         <div className="flex flex-wrap space-x-1 italic">
           <div className="flex space-x-1 items-center text-sm">
             <img className="h-6" src="/icon-followers.svg" />
@@ -48,28 +48,60 @@ const Sidebar: FC = () => {
         </div>
       </div>
       <div className="flex flex-col space-y-1 w-full px-3 text-white">
-        <div className="flex space-x-1 items-center">
-          <img className="h-6" src="/icon-building.svg" />
-          <p className="italic pl-2">{user.company || "Indisponível"}</p>
-        </div>
-        <div className="flex space-x-1 items-center">
-          <img className="h-6" src="/icon-location.svg" />
-          <p className="italic pl-2">{user.location || "Indisponível"}</p>
-        </div>
-        <div className="flex space-x-1 items-center">
-          <img className="h-6" src="/icon-mail.svg" />
-          <p className="italic pl-2">{user.email || "Indisponível"}</p>
-        </div>
-        <div className="flex space-x-1 items-center">
-          <img className="h-6" src="/icon-chain.svg" />
-          <p className="italic pl-2">{user.blog || "Indisponível"}</p>
-        </div>
-        <div className="flex space-x-1 items-center">
-          <img className="h-6" src="/icon-twitter.svg" />
-          <p className="italic pl-2">
-            {userHasTwitter ? `@${user.twitter_username}` : "Indisponível"}
-          </p>
-        </div>
+        {user.company ? (
+          <div className="flex space-x-1 items-center">
+            <img className="h-6" src="/icon-building.svg" />
+            <p className="italic pl-2">{user.company}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+        {user.location ? (
+          <div className="flex space-x-1 items-center">
+            <img className="h-6" src="/icon-location.svg" />
+            <p className="italic pl-2">{user.location}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+        {user.email ? (
+          <div className="flex space-x-1 items-center">
+            <img className="h-6" src="/icon-mail.svg" />
+            <p className="italic pl-2">{user.email}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+        {user.blog ? (
+          <Link href={user.blog}>
+            <a
+              className="flex space-x-1 items-center"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="h-6" src="/icon-chain.svg" />
+              <p className="italic pl-2 hover:underline cursor-pointer">
+                {user.blog}
+              </p>
+            </a>
+          </Link>
+        ) : (
+          <></>
+        )}
+        {user.twitter_username ? (
+          <Link href={`https://twitter.com/${user.twitter_username}`}>
+            <a
+              className="flex space-x-1 items-center hover:underline cursor-pointer"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img className="h-6" src="/icon-twitter.svg" />
+              <p className="italic pl-2">@{user.twitter_username}</p>
+            </a>
+          </Link>
+        ) : (
+          <></>
+        )}
       </div>
       <Link href="/">
         <button className="hidden sm:block bg-white text-gray-900 italic px-8 py-2 rounded-md">
